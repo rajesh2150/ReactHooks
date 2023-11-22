d020bbbd0391474b0f99b08a9eaf59ddd1a66f8d
Rajesh Korlapati <rajeshkorlapati465@gmail.com>
2023-11-22 21:30:41 +0530

    useState for count inc and dec as  commit 5

--- react-hooks/src/components/Hooks/useState/UseState.jsx
+++ react-hooks/src/components/Hooks/useState/UseState.jsx
@@ -2,11 +2,18 @@ import React,{useState} from 'react'
 
 const UseState = () => {
   const [name,setName]=useState('')
+  const [count,setCount]=useState(0)
   return (
     <div>
       <h3>This Is UseState</h3>
       <input type='text' onChange={(e)=>setName(e.target.value)}/>
       <h3>Hello,{name}</h3>
+      <hr/>
+      <div>
+        <h3>Count : {count}</h3>
+        <button onClick={()=>{setCount(count+1)}}>Increment</button>
+        <button onClick={()=>{setCount(count-1)}}>Decrement</button>
+      </div>
     </div>
   )
 }
