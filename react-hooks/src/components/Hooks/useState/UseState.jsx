4194435ccd5e685ed2d829241b8ffcd7e32a705f
Rajesh Korlapati <rajeshkorlapati465@gmail.com>
2023-11-22 21:28:11 +0530

    useState is defined and commit 4

--- react-hooks/src/components/Hooks/useState/UseState.jsx
+++ react-hooks/src/components/Hooks/useState/UseState.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const UseState = () => {
+  const [name,setName]=useState('')
   return (
     <div>
       <h3>This Is UseState</h3>
+      <input type='text' onChange={(e)=>setName(e.target.value)}/>
+      <h3>Hello,{name}</h3>
     </div>
   )
 }
